28b293c33de9ebee350f310e8037b424ec100b73
lotteeymann <86419683+lotteeymann@users.noreply.github.com>
2021-06-24 23:41:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,6 @@ var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-el.object3D.rotation.set(
-  THREE.Math.degToRad(15),
-  THREE.Math.degToRad(30),
-  THREE.Math.degToRad(90)
-);
 myOtherBox.object3D.rotation.x += rotationSpeed;
 console.log(myOtherBox.object3D.rotation.x);
 }
